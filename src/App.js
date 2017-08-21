87c39f6226d38bba02711c94fd20c711a4195f83
Tammy Dang <dangitstammy@gmail.com>
2017-08-21 11:39:51 -0700

    create state with two cards and a method to return cards at random

--- src/App.js
+++ src/App.js
@@ -3,6 +3,30 @@ import './App.css';
 import Card from './Card/Card';
 
 class App extends Component {
+  consstructor(props){
+    super(props);
+
+    this.state = {
+      cards: [
+        {id: 1, vocab: "Vocabulary", definition: "Definition"},
+        {id: 2, vocab: "Vocabulary_2", definition: "Definition_2"}
+      ],
+      currentCard: {}
+    }
+  }
+
+  componentWillMount(){
+    const currentCards = this.state.cards;
+
+    this.setState({
+      cards: currentCards,
+      currentCard: this.getRandomCard(currentCards)
+    })
+  }
+
+  getRandomCard(currentCards){
+    var card = currentCards[Math.floor(Math.random() * currentCards.length)]
+  }
   render() {
     return (
       <div className="App">
