91acd20e68959f5799404916be54d144df724c3c
Yousef Issa <yousef@u.northwestern.edu>
2019-01-31 12:04:38 -0600

    multiple message sending following ebbinghaus curve

--- server.js
+++ server.js
@@ -18,49 +18,80 @@ express_app.get('*', function(req, res){
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-express_app.post('/api/messages', (req, res) => {
+express_app.post('/api/messages', async (req, res) => {
     console.log('Received new message request.');
     res.header('Content-Type', 'application/json');
     console.log(req.body);
-    return res.send(scheduleText(req.body));
+    let messageCounter = 0;
+    req.body.ts.forEach(time => {
+        scheduleText(req.body, time);
+        messageCounter++;
+        console.log(scheduledTasks);
+    });
+    console.log({'success': true, 'messagesScheduled': messageCounter});
+    return res.send({'success': true, 'messagesScheduled': messageCounter});
 });
 
-const scheduleText = function(body) {
+const scheduleText = function(body, time) {
 
-    // create date object from post request
-    const sendTime = new Date(body.dueDate);
     const task_id = crypto.randomBytes(20).toString('hex');
     scheduledTasks.add(task_id);
 
-    // run job on specific node cluster
-    if (cluster.isMaster) {
-        // schedule text message send
-        const scheduled_task = scheduler.scheduleJob(sendTime, function () {
-            if (scheduledTasks.has(task_id)) {
-                // send text message with twilo service
-                client.messages
-                    .create({
-                        from: "+17574186902",
-                        to: body.to,
-                        body: body.message
-                    })
-                    .then(() => {
-                        console.log(`Message to ${body.to} successfully sent.`);
-                        console.log('Scheduled task canceled.');
-                        scheduled_task.cancel();
-                    })
-                    .catch(err => {
-                        console.log('hit error');
-                        console.log(err);
-                    });
-            } else {
-                console.log('Message already sent');
-            }
+    const scheduled_task = scheduler.scheduleJob(time, function () {
+        if (scheduledTasks.has(task_id)) {
+            // send text message with twilo service
+            client.messages
+                .create({
+                    from: "+17574186902",
+                    to: body.to,
+                    body: body.message
+                })
+                .then(() => {
+                    console.log(`Message to ${body.to} successfully sent.`);
+                    console.log('Scheduled task canceled.');
+                    scheduled_task.cancel();
+                })
+                .catch(err => {
+                    console.log('hit error');
+                    console.log(err);
+                });
+        } else {
+            console.log('Message already sent');
+        }
 
-        });
-        console.log('Text scheduled.');
-        return JSON.stringify({success: true});
-    }
+    });
+    console.log('Text scheduled.');
+    return JSON.stringify({success: true});
+
+    // // run job on specific node cluster
+    // if (cluster.isMaster) {
+    //     // schedule text message send
+    //     const scheduled_task = scheduler.scheduleJob(time, function () {
+    //         if (scheduledTasks.has(task_id)) {
+    //             // send text message with twilo service
+    //             client.messages
+    //                 .create({
+    //                     from: "+17574186902",
+    //                     to: body.to,
+    //                     body: body.message
+    //                 })
+    //                 .then(() => {
+    //                     console.log(`Message to ${body.to} successfully sent.`);
+    //                     console.log('Scheduled task canceled.');
+    //                     scheduled_task.cancel();
+    //                 })
+    //                 .catch(err => {
+    //                     console.log('hit error');
+    //                     console.log(err);
+    //                 });
+    //         } else {
+    //             console.log('Message already sent');
+    //         }
+    //
+    //     });
+    //     console.log('Text scheduled.');
+    //     return JSON.stringify({success: true});
+    // }
 };
 
 express_app.portNumber = 3002;
