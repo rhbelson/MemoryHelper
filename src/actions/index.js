65f89969715fed2a7a5cdfd616ee39e18aab78fc
Yousef Issa <yousef@u.northwestern.edu>
2019-02-05 13:12:52 -0600

    updated reminder

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,13 +1,12 @@
 import { ADD_REMINDER, DELETE_REMINDER, DELETE_ALL_REMINDERS, TOGGLE_MODAL } from '../constants';
 
-export const addReminder = (text, numReminders, dueDate, phone, modal) => {
+export const addReminder = (text, dueDate, phone, modal) => {
   const action = {
     type: ADD_REMINDER,
     text,
     dueDate,
     phone,
     modal,
-    numReminders: numReminders + 1
   };
   return action;
 }
@@ -23,7 +22,6 @@ export const deleteReminder = (id) => {
 export const deleteAllReminders = () => {
   const action = {
     type: DELETE_ALL_REMINDERS,
-    numReminders: 0
   };
   return action;
 }
